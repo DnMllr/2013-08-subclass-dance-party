f9f549609f8cea758832dc74a7bf04b9e8c5ac52
DnMllr <yajnavalkya@cox.net>
2013-08-13 10:10:00 -0700

    angle and distance implimented

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -6,12 +6,17 @@ var BlinkyDancer = function(top, left, timeBetweenSteps){
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
 BlinkyDancer.prototype.constructor = BlinkyDancer;
 
+BlinkyDancer.prototype.calculateDistance =   function (elem, mouseX, mouseY) {
+      return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2)));
+  }
+
 BlinkyDancer.prototype.step = function(){
-  console.log('hi');
   // call the old version of step at the beginning of any call to this new version of step
   Dancer.prototype.step.call(this);
+  
   /* toggle() is a jQuery method to show/hide the <span> tag.
    * See http://api.jquery.com/category/effects/ for this and
    * other effects you can use on a jQuery-wrapped html tag. */
-  this.$node.toggle();
+  //this.$node.toggle();
+
 };
--- src/dancer.js
+++ src/dancer.js
@@ -5,7 +5,7 @@ var Dancer = function(top, left, timeBetweenSteps){
   this.$node = $('<span class="dancer"></span>');
   this.test = "test";
   this.step();
-
+  this.distance = 0;
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
@@ -15,9 +15,8 @@ var Dancer = function(top, left, timeBetweenSteps){
 Dancer.prototype.step = function(){
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  console.log(this);
-  var that = this;
-  setTimeout(function() { that.step(); }, this.timeBetweenSteps);
+ var that = this;
+ setTimeout(function() { that.step(); }, this.timeBetweenSteps);
 };
 
 Dancer.prototype.setPosition = function(top, left){
